fb6d719a468c46e2e9808af113add8baf47f46fb
NEERAJ <6620913@gmail.com>
2022-02-28 22:32:46 +0530

    prototype is ready

---
+++ static/script.js
@@ -0,0 +1,24 @@
+
+
+const decr_numbers = document.querySelectorAll('.number_effect'); // select elements from document with class name number_effect
+const decr_by = 1;  // number by which the number is decreased each time
+
+
+decr_numbers.forEach(number => {
+
+    const updateNumber = () => {
+
+        const target = number.getAttribute('abc'); // get target value from attribute 'abc' 
+        const num = number.innerText;
+        dcr = decr_by;
+        if (num > target) {
+            number.innerText = num - dcr;
+            setTimeout(updateNumber, 100);
+        }
+        else {
+            number.innerText = target;
+        }
+    };
+    updateNumber();
+});
+
