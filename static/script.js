15e6a84dba518ffbd3a1afd3023efe5fa8cf3a62
NEERAJ <6620913@gmail.com>
2022-03-05 14:51:34 +0530

    prototype compleat

--- static/script.js
+++ static/script.js
@@ -1,18 +1,26 @@
 
 
-const decr_numbers = document.querySelectorAll('.number_effect'); // select elements from document with class name number_effect
-const decr_by = 1;  // number by which the number is decreased each time
+// The given function will be responsible for the changing count in counter
+function counter() {
+    
+    const decr_by = 1;  // number by which the number is decreased each time
 
-
-decr_numbers.forEach(number => {
 
     const updateNumber = () => {
+        const number = document.getElementById("cp")
 
         const target = number.getAttribute('abc'); // get target value from attribute 'abc' 
         const num = number.innerText;
         dcr = decr_by;
         if (num > target) {
-            
+            if (num <= 100) {
+                document.getElementById("lh").style.display = "block";
+                console.log("lH");
+            }
+            if (num < 10) {
+                document.getElementById('lt').style.display = 'block';
+            }
+
             number.innerText = num - dcr;
             setTimeout(updateNumber, 500);
         }
@@ -21,5 +29,30 @@ decr_numbers.forEach(number => {
         }
     };
     updateNumber();
-});
+
+
+}
+
+
+
+const ldigit = document.querySelector(".n03");
+
+const digit = document.querySelectorAll(".digit");
+
+
+ldigit.onanimationend = () => {
+    console.log('Animation ended');
+    show();
+};
+
+function show() {
+    
+    document.getElementById("np").style.display = "none";
+    document.getElementById("dp").style.display = "flex";
+    counter();
+
+
+   
+};
+
 
